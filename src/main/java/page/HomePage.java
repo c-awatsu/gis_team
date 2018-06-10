f920fd6d4076908af9d36935e8651749720ea612
c-awatsu <cist.b213.c.awatsu@gmail.com>
2018-06-10 16:41:50 +0900

    微修正

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -1,24 +1,3 @@
-### 1.Leafletで地図を表示してみる
-
-`java/page`パッケージに以下のファイルを作る 
-
-HomePage.html
-```html
-<!DOCTYPE  html>
-<html xmlns:wicket="http:/wicket.apache.org">
-<head>
-    <meta charset="UTF-8"/>
-    <title>MapPage</title>
-</head>
-<body>
-    <div style="width: 600px;height: 600px;" id="map"></div>
-</body>
-</html>
-
-```
-
-HomePage.java
-```java
 package page;
 
 import de.agilecoders.wicket.webjars.request.resource.WebjarsCssResourceReference;
@@ -29,7 +8,7 @@ import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 
-public class MapPage extends WebPage {
+public class HomePage extends WebPage {
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
@@ -40,28 +19,3 @@ public class MapPage extends WebPage {
         response.render(OnDomReadyHeaderItem.forScript("drawMap();"));
     }
 }
-
-```
-
-`webapp/js`パッケージを作成し以下のファイルを作る
-
-```javascript
-var map;
-var tileLayer;
-var staticLat = 42.828816;
-var staticLon = 141.650705;
-
-function drawMap() {
-    map = L.map('map').setView([staticLat, staticLon], 13);
-
-    tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-        maxZoom: 19
-    });
-
-    tileLayer.addTo(map);
-}
-```
-
-**実行結果**
-![地図]()
