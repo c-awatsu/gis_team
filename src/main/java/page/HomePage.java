de7d88c05b258994dc1939ef7f35b63205fcc2f6
c-awatsu <cist.b213.c.awatsu@gmail.com>
2018-06-12 12:25:26 +0900

    handsOn用に変更

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -1,22 +1,7 @@
 package page;
 
-import de.agilecoders.wicket.webjars.request.resource.WebjarsCssResourceReference;
-import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
-import net.arnx.jsonic.JSON;
-import org.apache.wicket.markup.head.CssHeaderItem;
-import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
-    @Override
-    public void renderHead(IHeaderResponse response) {
-        super.renderHead(response);
-        response.render(JavaScriptHeaderItem.forReference(getApplication().getJavaScriptLibrarySettings().getJQueryReference()));
-        response.render(JavaScriptHeaderItem.forReference(new WebjarsJavaScriptResourceReference("./leaflet/current/dist/leaflet.js")));
-        response.render(CssHeaderItem.forReference(new WebjarsCssResourceReference("./leaflet/current/dist/leaflet.css")));
-        response.render(JavaScriptHeaderItem.forUrl("./js/map.js"));
-        response.render(OnDomReadyHeaderItem.forScript("drawMap("+ JSON.encode("引数で渡した内容が表示されます") +");"));
-    }
+
 }
